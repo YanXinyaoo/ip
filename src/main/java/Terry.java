b0d2401d4a1c106427cfa87c91678f2a001bf258
YanXinyaoo <e1155531@u.nus.edu>
2025-01-21 18:12:34 +0800

    Rename Duke.java to Terry.java and remove all contents in Duke.java. Rename the chatbot as Terry and Implement the preliminary feature of greeting and exiting

---
+++ src/main/java/Terry.java
@@ -0,0 +1,13 @@
+public class Terry {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Terry");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        String userInput = "bye";
+
+        if ("bye".equalsIgnoreCase(userInput)) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+        }
+    }
+}
