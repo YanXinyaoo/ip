ae80c261a32e29d428f34407db75d07aabdce5cb
YanXinyaoo <e1155531@u.nus.edu>
2025-01-29 15:40:41 +0800

    It echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Terry.java
+++ src/main/java/Terry.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class Terry {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Terry");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        String userInput = "bye";
+        Scanner sc = new Scanner(System.in);
+        String dottedLine = "    ____________________________________________________________";
+        System.out.println(dottedLine);
+        System.out.println("    Hello! I'm Terry");
+        System.out.println("    What can I do for you?");
+        System.out.println(dottedLine);
 
-        if ("bye".equalsIgnoreCase(userInput)) {
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("____________________________________________________________");
+        String userInput;
+        while(sc.hasNext()) {
+            userInput = sc.nextLine();
+            System.out.println(dottedLine);
+            if ("bye".equalsIgnoreCase(userInput)) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println(dottedLine);
+                break;
+            } else {
+                System.out.println("    " + userInput);
+                System.out.println(dottedLine);
+            }
         }
     }
 }
