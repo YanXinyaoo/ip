98b5bd3005d21f983358b34bcecc48803f4f6a2f
YanXinyaoo <e1155531@u.nus.edu>
2025-01-29 16:04:17 +0800

    Add feature of adding to a list and showing the list. If the list is empty will have different notification.

--- src/main/java/Terry.java
+++ src/main/java/Terry.java
@@ -4,23 +4,40 @@ public class Terry {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String dottedLine = "    ____________________________________________________________";
+        String[] tasks = new String[100];
+        String bye = "bye";
+        String list = "list";
+        int currentTask = 0;
+
         System.out.println(dottedLine);
         System.out.println("    Hello! I'm Terry");
         System.out.println("    What can I do for you?");
         System.out.println(dottedLine);
 
-        String userInput;
         while(sc.hasNext()) {
-            userInput = sc.nextLine();
+            String userInput = sc.nextLine();
+            if(userInput.equals("")) { //avoid empty line
+                continue;
+            }
             System.out.println(dottedLine);
-            if ("bye".equalsIgnoreCase(userInput)) {
+            if (bye.equalsIgnoreCase(userInput)) { //bye
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println(dottedLine);
                 break;
-            } else {
-                System.out.println("    " + userInput);
-                System.out.println(dottedLine);
+            } else if(list.equalsIgnoreCase(userInput)) { //list
+                if (currentTask == 0) {
+                    System.out.println("    Chill! No task!");
+                } else {
+                    for (int i = 0; i < currentTask; i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else { //add
+                tasks[currentTask] = userInput;
+                currentTask++;
+                System.out.println("    added: " + userInput);
             }
+            System.out.println(dottedLine);
         }
     }
 }
