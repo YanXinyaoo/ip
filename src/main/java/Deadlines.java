180db8ceada9d99c7514d68dccf6519922c7d32a
YanXinyaoo <e1155531@u.nus.edu>
2025-02-05 13:11:56 +0800

    Add support for tracking three types of tasks: todos, deadlines, events

---
+++ src/main/java/Deadlines.java
@@ -0,0 +1,13 @@
+public class Deadlines extends Task{
+    private String ddl;
+
+    public Deadlines(String description, String ddl) {
+        super(description);
+        this.ddl = ddl;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + ddl + ")";
+    }
+}
--- src/main/java/Terry.java
+++ src/main/java/Terry.java
@@ -1,64 +1,59 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Terry {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String dottedLine = "    ____________________________________________________________";
-        ArrayList<Task> tasks = new ArrayList<>();
         String bye = "bye";
         String list = "list";
-        int currentTask = 0;
 
         System.out.println(dottedLine);
         System.out.println("    Hello! I'm Terry");
         System.out.println("    What can I do for you?");
         System.out.println(dottedLine);
 
-        while(sc.hasNext()) {
+        TaskManager taskManager = new TaskManager();
+
+        while (sc.hasNext()) {
             String userInput = sc.nextLine().trim();
             String[] parts = userInput.split(" ", 2);
             String command = parts[0].toLowerCase();
-            Integer commandNumber = Integer.parseInt(parts.length > 1 ? parts[1] : "0");
             if (userInput.isEmpty()) continue; //skip empty line
 
             System.out.println(dottedLine);
+
             switch (command) {
             case "bye":
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println(dottedLine);
                 return;
             case "list":
-                if (tasks.size() == 0) {
-                    System.out.println("    Chill! No task!");
-                } else {
-                    System.out.println("    Here are the tasks in your list");
-                    for (Task task : tasks) {
-                        int index = tasks.indexOf(task);
-                        System.out.println("    " + (index + 1) + "." + task.toString());
-                    }
-                }
+                taskManager.listTasks();
                 break;
             case "mark":
-                if (commandNumber > 0 && commandNumber < tasks.size()) {
-                    System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("      " + tasks.get(commandNumber).markAsDone());
-                } else {
-                    System.out.println("    Invalid task number!");
-                }
+                Integer markNumber = Integer.parseInt(parts.length > 1 ? parts[1] : "0");
+                taskManager.markTaskAsDone(markNumber);
                 break;
-
             case "unmark":
-                if (commandNumber > 0 && commandNumber < tasks.size()) {
-                    System.out.println("    OK, I've marked this task as not done yet:");
-                    System.out.println("      " + tasks.get(commandNumber).markAsUndone());
-                } else {
-                    System.out.println("    Invalid task number!");
-                }
+                Integer unmarkNumber = Integer.parseInt(parts.length > 1 ? parts[1] : "0");
+                taskManager.markTaskAsUndone(unmarkNumber);
+                break;
+            case "todo":
+                taskManager.addTask(new ToDos(parts[1]));
+                break;
+            case "deadline":
+                String[] partDeadline = userInput.split("/by ", 2);
+                taskManager.addTask(new Deadlines(partDeadline[0], partDeadline[1]));
+                break;
+            case "event":
+                String[] partEvent = userInput.split("/from|/to");
+                taskManager.addTask(new Events(partEvent[0], partEvent[1], partEvent[2]));
                 break;
             default:
-                tasks.add(new Task(userInput));
-                System.out.println("    added: " + userInput);
             }
             System.out.println(dottedLine);
         }
